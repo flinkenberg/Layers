eecea0e2fb467d3d23d736fabb96f9227925523a
flinx <stastny.gr@gmail.com>
2016-03-23 21:03:37 +0000

    Custom smooth transition styling

--- src/js/layers.js
+++ src/js/layers.js
@@ -2,15 +2,16 @@ var Wraps = document.getElementsByClassName('layer');
 var Layers = document.getElementsByClassName('layer-content');
 
 function LayersScroll() {
+  var triggerPoint = 1;
   for (var i = 0; i < Layers.length; i++) {
     if (Wraps[i].scrollHeight > document.documentElement.clientHeight) {
       if ((Wraps[i].offsetTop+Wraps[i].scrollHeight - window.scrollY) < document.documentElement.clientHeight) {
-        !Wraps[i].className.match(/(?:^|\s)pinned(?!\S)/) ? Wraps[i].className += ' pinned' : null;
+        !Wraps[i].className.match(/(?:^|\s)pinnedBottom(?!\S)/) ? Wraps[i].className += ' pinnedBottom' : null;
       } else {
-        Wraps[i].className =  Wraps[i].className.replace(/(?:^|\s)pinned(?!\S)/g, '')
+        Wraps[i].className =  Wraps[i].className.replace(/(?:^|\s)pinnedBottom(?!\S)/g, '')
       }
     } else {
-      if (window.scrollY > Wraps[i].offsetTop) {
+      if (window.scrollY > Wraps[i].offsetTop+triggerPoint) {
         !Wraps[i].className.match(/(?:^|\s)pinned(?!\S)/) ? Wraps[i].className += ' pinned' : null;
       } else {
         Wraps[i].className =  Wraps[i].className.replace(/(?:^|\s)pinned(?!\S)/g, '')
